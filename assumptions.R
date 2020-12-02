17a6caba5c19644bc905ee2300185f4bc8539847
James <james.baxter@ed.ac.uk>
2020-12-02 17:21:25 +0000

    main function and condensed correction function

--- assumptions.R
+++ assumptions.R
@@ -10,11 +10,13 @@ formatDF <- function(df){
   if ('multiple.founders' %in% colnames(df)){
     no_na <- df[!is.na(df$multiple.founders),]
     labelled <-  unite(no_na, "publication", c(author ,year), sep = '_')
+    
   }else{
     labelled <- NULL
     stop('no founder multiplicity column found')
   }
   formatted <- labelled
+  
   return(formatted)
 }
 
@@ -23,6 +25,7 @@ formatDF <- function(df){
 groupDF <- function(df, covar){
   if (class(df$multiple.founders)=="factor"){
     df$multiple.founders = 1 - (as.numeric(df$multiple.founders)-1)
+    
   }else{
     stop('founder multiplicity is not a factor')
   }
@@ -32,6 +35,7 @@ groupDF <- function(df, covar){
     df_grouped <- df %>% 
       group_by(publication) %>%
       summarise(subjects = n(), multiple.founders = sum(multiple.founders))
+    
   }else{
     df_grouped <- df %>% 
       group_by(publication, sym(covar)) %>%
@@ -43,10 +47,12 @@ groupDF <- function(df, covar){
 
 #continuity correction for log transformations where proportions = 0 or 1. correction set at 0.3, the expected proportion of founder variants from previous studies
 contCorrection <- function(x){
-  if(x==0|x==1){
-    x=0.3
+  if(x == 0 | x == 1){
+    x <- 0.3
+    
   }else{
-        x=x}
+    x <- x}
+  
   return(x)
 }
 
@@ -55,19 +61,24 @@ contCorrection <- function(x){
 calcProps <- function(df, logtransfom = TRUE){
   if ('multiple.founders' %in% colnames(df)){
     props.init <- df$multiple.founders/df_grouped$subjects
+    
   }else{
     stop('no founder multiplicity column found')
   }
+  
   if (logtransfom == TRUE){
     print('calculating log transformed proportions')
     props.corrected <- lapply(props , contCorrection) %>% unlist()
     props <- log(props.corrected)
+    
   }else if (logtransform == FALSE){
     print('calculating raw proportions')
     props <- props.init
+    
   }else{
     stop('specify whether a log transform should be applied by stating logtransfom = TRUE or logtransfom = FALSE')
   }
+  
   return(props)
 }
 
@@ -79,19 +90,29 @@ assumption1 <- function(){
   gofstat(fit.norm.cont)
 }
 
-
-
       
 #exploring assumption 2: evaluating independence of covariates
 assumption1 <- function()
   
   
 #main
-main <- function(){
-  if (data != class(data.frame))
+main <- function(data, covar, logtransform = TRUE ){
+  if (data != class(data.frame)){
+    stop('input is not a data frame')
+    
+  }else{
+  data = data
+  }
+  
+  props <- formatDF(data) %>%
+    groupDF(.,covar) %>%
+    calcProps(., logtransform = )
+
+  assumption1(props)
+  assumption2(props)
 }  
 
 #import dataset
 data_master<- read.csv("data_master.csv", na.strings = "NA")
-
+main(data_master)
 
