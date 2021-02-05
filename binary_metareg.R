3476dad091abbdff177b1a62735029766292c2ff
James <james.baxter@ed.ac.uk>
2021-02-05 20:05:57 +0000

    altered dependencies, outlined modelling code with preliminary models

--- binary_metareg.R
+++ binary_metareg.R
@@ -17,21 +17,22 @@ library(tidyr)
 library(lme4)
 library(dplyr)
 library(mltools)
-library(data.table)
-library(metafor)
-library(dmetar)
-library(aod)
 library(ggplot2)
 library(influence.ME)
 library(kableExtra)
+library(parallel)
+library(performance)
 source('generalpurpose_funcs.R')
 
 # One-step GLMM accounting for clustering of studies using a random intercept
 CalcRandMetaReg <- function(data, formula){
-  model <- glmer(formula,
+  model <- lme4::glmer(formula,
                  data = data,
                  family = binomial(link = "logit"),
-                 control = glmerControl(optimizer="bobyqa", optCtrl = list(maxfun = 100000)))
+                 nAGQ = 1,
+                 control = glmerControl(optCtrl = list(maxfun = 100000),
+                                        check.nobs.vs.nlev = 'ignore',
+                                        check.nobs.vs.nRE = 'ignore'))
   return(model)
 }
 
@@ -42,19 +43,49 @@ CalcRandMetaReg <- function(data, formula){
 setwd("./data")
 df <- read.csv("data_master_11121.csv", na.strings = "NA") %>% formatDF()
 
+# Set seed
 set.seed(4472)
 
 ###################################################################################################
-f0 <- as.formula("multiple.founders ~  1 + (1|publication) + (0 + 1|publication)")
-f1 <- as.formula("multiple.founders ~  reported.exposure + (reported.exposure + 1 ||publication) -1")
-f2 <- as.formula("multiple.founders ~  direction::reported.exposure + (reported.exposure + 1 ||publication) -1")
-test_reg <- CalcRandMetaReg(df, f1)
 
-props_metareg <- CalcProps(df, reported.exposure)
+# Outline formulas for meta-regression. 
+forms <- c(f0 = as.formula("multiple.founders ~  1 + (1|publication)"),
+           f1 = as.formula("multiple.founders ~  riskgroup + (1| cohort) + (1 |publication) -1"),
+           f2 = as.formula("multiple.founders ~  riskgroup + grouped.method + (1| cohort) + (1 |publication) -1"),
+           f3 = as.formula("multiple.founders ~  riskgroup + (1|grouped.method) + (1| cohort) + (1 |publication) -1"),
+           f4 = as.formula("multiple.founders ~  riskgroup + grouped.method + sequencing.region + (1| cohort) + (1 |publication) -1"),
+           f5 = as.formula("multiple.founders ~  reported.exposure + grouped.method + sequencing.region  + (1| cohort) + (1 |publication) -1")
+           )
 
-# Model checklist:
-# 1. Convergence
-# 2. 
+# Set up cluster
+cl <- detectCores() %>%
+  `-` (2) %>%
+  makeCluster()
+
+clusterEvalQ(cl,library(lme4))
+
+# Set time and run models
+start <- Sys.time()
+start
+
+test_reg <- parLapply(cl = cl ,forms, CalcRandMetaReg , data = df_splittrans)
+
+end <- Sys.time()
+elapsed <- end-start
+elapsed
+
+stopCluster(cl)
+remove(cl)
+
+###################################################################################################
+###################################################################################################
+
+# Model Evaluation
+# 1. Check Convergence
+# 2. Binned residuals (ideally >95% within SE)
+
+# 1. Check Convergence
+
 
 #plot to check binned residuals
 binned <- performance::binned_residuals(test_reg)
@@ -73,12 +104,13 @@ ggplot(data = binned) +
   theme(legend.position = "none")
 
 #plot distribution of within study estimates (eg forest plot) next to modelled modifier
-ran.eff <- ranef(test_reg)[[1]] %>% gather()
+ran.eff <- ranef(test_reg_2)[[1]] %>% gather()
 ran.eff$key <- gsub("reported.exposure", "", ran.eff$key)
 ran.df <- ran.eff[ran.eff$key != "(Intercept)", ]
-ggplot(data = ran.df,aes( value, key)) + geom_boxplot() +theme_classic()
+ggplot(data = ran.df,aes( value, key)) + geom_boxplot() +theme_classic() 
+fix.eff <- cbind.data.frame(gsub("reported.exposure", "", test_reg@cnms[[2]]),fixef(test_reg))
 
-
+ggplot(data = fix.eff) + geom_point(aes(y= exposure, x = estimate)) +theme_classic
 #compare crude model to moderator model
 
 split_transission(props_metareg ,names)
@@ -97,25 +129,3 @@ step2 <- rma.uni(yi, vi,
                  )
 ggplot(data = df_props , aesx = reported.exposure, y = (multiplefounders/subjects)) +geom_density_ridges()
 
-
-maybe <- cbind.data.frame(test_reg@beta, )
-
-
-
-
-ggplot(cbind.data.frame("exposure" = test_reg@cnms[[2]], "estimate" = transf.ilogit(test_reg@beta)),aes(x= exposure, y = estimate)) +
-  geom_point( shape = 4, size = 4) + 
-  scale_y_continuous(name = "Pooled Proportion of Founder Variant Multiplicity",limits=c(0,.5),expand = c(0.01, 0.01)) +
-  scale_x_discrete()+
-  theme_classic() + 
-  coord_flip() +
-  geom_linerange(aes(ymin=estimate.lb, ymax= estimate.ub, color = analysis), position = position_dodge(0.5)) +
-  scale_color_npg(name = NULL, labels = c(
-    no_small = "Studies with (n<10) omitted",
-    no_zeros = "Studies with (p=0) omitted",
-    original = "Full analysis")) + 
-  theme(legend.position = "bottom",
-        axis.text = element_text(size = 10.5,  family = "sans"),
-        legend.text = element_text(size = 10.5,  family = "sans"),
-        axis.title = element_text(size = 13,  family = "sans")
-  )
