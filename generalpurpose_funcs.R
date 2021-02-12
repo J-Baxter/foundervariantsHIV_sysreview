5b6fc606a6c22bfed8ef55150135fc115bf21953
James <james.baxter@ed.ac.uk>
2021-02-12 20:42:06 +0000

    added filter option for covariates with less than 5 data points

--- generalpurpose_funcs.R
+++ generalpurpose_funcs.R
@@ -6,23 +6,47 @@
 ###################################################################################################
 
 # Formats data spreadsheet for analysis. Removes duplicates and NAs.
-formatDF <-  function(df, noreps = TRUE){
+# noreps removes repeated measurements (reason for false would be to conduct sensitivity analyses)
+# filter removes factors containing less than 5 individuals from specified df
+formatDF <-  function(df, noreps = TRUE, filter = NULL){
   require(tidyr)
   require(reshape2)
+  
   #create dummy variables in founder multiplicity col
   if (class(df$multiple.founders)=="factor"){
     df$multiple.founders = 1 - (as.numeric(df$multiple.founders)-1)
+    
   }else{
     stop('founder multiplicity is not a factor')
   }
+  
   df_nona <- df[!is.na(df$multiple.founders),]
+  
   if (noreps == TRUE){
     df_nodups <- df_nona[(df_nona$include.main == '') & (df_nona$exclude.repeatstudy == ''),]
     df_labelled <- unite(df_nodups, "publication", c(author ,year), sep = '_')
+    
   }else{
     df_labelled <- unite(df_nona, "publication", c(author ,year), sep = '_')
   }
-  df_splittrans <- colsplit(df_labelled$reported.exposure, ":" , c("riskgroup" , "direction")) %>% cbind.data.frame(.,df_labelled)
+  
+  if(is.character(filter)){
+    looped_df <- df_labelled
+    for (i in 1:length(filter)){
+      fac <- filter[i]
+      tbl <- table(looped_df[fac])
+      looped_df <- looped_df[!looped_df[[fac]] %in% names(tbl)[tbl < 5],]
+    }
+    
+    df_splittrans <- colsplit(looped_df$reported.exposure, ":" , c("riskgroup" , "direction")) %>%
+      cbind.data.frame(.,looped_df)
+    
+  }else{
+    df_splittrans <- colsplit(df_labelled$reported.exposure, ":" , c("riskgroup" , "direction")) %>%
+      cbind.data.frame(.,df_labelled)
+  }
+
+  
   return(df_splittrans)
 }
 
