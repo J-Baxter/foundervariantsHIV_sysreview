d7476f058c45bb97928281a8b1664923b40337b5
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2021-02-24 21:20:49 +0000

    Delete citation_dag.R

--- citation_dag.R
+++
@@ -1,8 +0,0 @@
-#citation DAG
-#generate directed acyclic graph from citations of publications included in systematic review
-
-#dependencies
-library(ggnet)
-library(bibliometrix)
-
-#requires bibtex input 
