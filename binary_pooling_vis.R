251df009e98d56de91b6d93c55dd512c5feaadbb
James <james.baxter@ed.ac.uk>
2021-05-01 23:13:37 +0100

    reorganised plots

--- binary_pooling_vis.R
+++ binary_pooling_vis.R
@@ -52,19 +52,16 @@ PltBoot <- function(data, intercept, ci.lb, ci.ub){
 ###################################################################################################
 ###################################################################################################
 # Import data
-setwd("./data")
+influence_df <- read.csv("./results/pooling_sa1.csv") %>% arrange(., model)
 
-influence_df <- read.csv("bp_sa1.csv") %>% arrange(., model)
+pooled_models <-  read.csv('./results/pooling_estsa2sa3sa4.csv')
 
-pooled_models <-  read.csv('bp_estsa2sa3sa4.csv')
+resampled_models <- read.csv('./results/pooling_boot.csv')
 
-resampled_models <- read.csv('bp_resampl.csv')
+models <- c('Two-Step Binomial-Normal',
+            'One-Step Binomial GLMM')
 
-models <- c('Two-Step Binomial Normal',
-            'One-Step Binomial',
-            "Two-Step Beta-Binomial")
-
-og_models <- cbind("model" = pooled_models[1:3,1], pooled_models[1:3,3:9] %>% round(digits = 3)) %>% arrange(., model)
+og_models <- cbind("model" = pooled_models[1:2,1], pooled_models[1:2,3:8] %>% round(digits = 3)) %>% arrange(., model)
 
 ###################################################################################################
 ###################################################################################################
@@ -73,31 +70,24 @@ og_models <- cbind("model" = pooled_models[1:3,1], pooled_models[1:3,3:9] %>% ro
 og_models_formatted <- cbind.data.frame("Estimate" = paste0(og_models$estimate, ' ' ,
                                                                '[' , og_models$estimate.lb ,' - ',
                                                                og_models$estimate.ub, ']'),
-                                        og_models[,5:8],
-                                        row.names =models ) 
+                                        og_models[,5:7],
+                                        row.names = rev(models) ) 
 og_models_formatted[is.na(og_models_formatted )] <- "-"
 
 # Not hapy with formatting
 tbl <- kbl(og_models_formatted , 
           booktabs = T,
-           col.names = c('Estimate', '$$\\hat{\\tau}^2$$', "$$\\text{Q}$$", "$$\\text{I}^2$$", "$$\\phi$$"),
+           col.names = c('Estimate', '$$\\hat{\\tau}^2$$', "$$\\text{Q}$$", "$$\\text{I}^2$$"),
            escape = FALSE,
            align = 'c', 
            ) %>%
-  add_header_above(c(" " = 1, "Probability of Multiple Founders" = 1, "Heterogeneity" = 4), bold = T, line = T) %>%
+  add_header_above(c(" " = 1, "Probability of Multiple Founders" = 1, "Heterogeneity" = 3), bold = T, line = T) %>%
  kable_classic(full_width = F, html_font = 'arial')
-
-  
-###################################################################################################
-# Plot log odds of individual studies. should be normally distiributed to satisfy binomial-normal model.
-ggplot(twostep_binorm.step1, aes(x=log_or)) + geom_histogram(binwidth = 0.25,color="black", fill="white")+
-  theme_classic() + scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0))
 
 
 ###################################################################################################
 ###################################################################################################
 # Panel: Sensitivity analyses (exclusion criteria and resampling)
-
 
 # Exclusion critera dot and whisker plot
 senseplot <- ggplot(pooled_models,
@@ -113,11 +103,10 @@ senseplot <- ggplot(pooled_models,
   
   scale_x_discrete(name = "Model", 
                    labels = c(
-                     onestep_bi_rand= "GLM",
-                     twostep_betabi = "BB",
-                     twostep_binorm = "BN"
+                     onestep_bi_rand = "GLMM",
+                     twostep_binorm = "B-N"
                      )) +
-  theme_classic() + 
+  theme_bw() + 
   
   coord_flip() +
   
@@ -126,33 +115,67 @@ senseplot <- ggplot(pooled_models,
                      color = analysis), 
                  position = position_dodge(0.5)) +
   
-  scale_color_viridis_d(name = NULL, labels = c(
+  scale_color_npg(name = 'Analysis', labels = c(
     no_small = "Studies with (n<10) omitted",
     no_zeros = "Studies with (p=0) omitted",
     original = "Full analysis",
     sga_only = 'Only SGA sequences')) + 
   
-  theme(legend.position = "bottom",
-        axis.text = element_text(size = 10.5,  family = "sans"),
-        legend.text = element_text(size = 10.5,  family = "sans"),
-        axis.title = element_text(size = 13,  family = "sans")#,
+  theme(legend.position = c(0.8,0.86),
+        axis.text = element_text(size = 9.5,  family = "sans"),
+        legend.text = element_text(size = 9.5,  family = "sans"),
+        axis.title = element_text(size = 11,  family = "sans"),
+        legend.background = element_blank()#,
         #plot.margin = unit(c(2,4,2,1), "lines")
   )
 
-# Resampling histograms - requires model input from binary_pooling.R
-resampled_models.list <- split.data.frame(resampled_models, resampled_models$analysis) 
 
-plt_boot.list <- mapply(PltBoot, data = resampled_models.list, 
-                        intercept = og_models[,2], 
-                        ci.lb = og_models[,3],
-                        ci.ub = og_models[,4],
-                        SIMPLIFY = FALSE)
+boot_plt <- ggplot(resampled_models) + 
+  geom_histogram(aes(x = estimate,
+                 color=analysis, 
+                 fill=analysis),
+                 binwidth = 0.0005) + 
+  
+  #geom_vline(aes(xintercept=mean(estimate)),color="#DC0000B2", linetype="dashed", size=1) +
+  geom_vline(aes(xintercept= estimate,
+            color= model),
+             linetype="dashed", 
+             size=0.75, 
+            data = og_models) +
+  
+  theme_bw() +
+  
+  scale_y_continuous(name = "Frequency" ,
+                     limits=c(0,75),
+                     expand = c(0, 0)) +
+  
+  scale_x_continuous(name = "Probability of Multiple Founders",
+                     limits=c(0,0.5), 
+                     expand = c(0, 0))+
 
-plt_boot.grid <- cowplot::plot_grid(plotlist = plt_boot.list , align = "hv" , nrow = 3, labels = c("B" , 'C' , "D" , "E"))
+  geom_rect(data = og_models,
+            aes(ymin=0, ymax=Inf,
+            xmin=estimate.lb, xmax=estimate.ub,fill = model),alpha = .1) +
+  scale_color_npg(name = c(analysis = 'Model'),
+                  labels = c(
+    onestep_bi_rand = "One-Step GLMM",
+    twostep_binorm = "Two-Step Binomial Normal"))+
+  scale_fill_npg(name = c(analysis = 'Model'),
+                 labels = c(
+    onestep_bi_rand = "One-Step GLMM",
+    twostep_binorm = "Two-Step Binomial Normal"))+
+  
+  theme(axis.text = element_text(size = 9.5,  family = "sans"),
+        legend.text = element_text(size = 9.5,  family = "sans"),
+        axis.title = element_text(size = 11,  family = "sans"),
+        legend.position = c(0.82,0.9),
+        legend.background = element_blank())
 
-jpeg("pooling_sa.jpeg" ,width = 4500, height = 4000, res = 380 ,units = "px", pointsize = 12)
+
+
+jpeg("./results/pooling_sa.jpeg" ,width = 5000, height = 2500, res = 380 ,units = "px", pointsize = 12)
 cowplot::plot_grid(senseplot, 
-                   plt_boot.grid , ncol = 2,  rel_widths  = c(1, 0.5) ,labels = "A", align = 'h', axis = 'b', greedy = F)
+                   boot_plt , ncol = 2,  rel_widths  = c(1,1) ,labels = "AUTO", align = 'h', axis = 'b', greedy = F)
 dev.off()
 
 
@@ -169,15 +192,14 @@ plt <- ggplot(influence_df,aes(x = trial , y = estimate) ) +
   
   scale_x_discrete(labels = influence_df$trial) +
   
-  theme_classic() + 
+  theme_bw() + 
   
   coord_flip() +
   
   facet_grid(cols = vars(model),
              labeller = labeller(model = c(
-               twostep_binorm = "Binomial-Normal",
-               twostep_betabi = "Beta-Binomial",
-               onestep_bi_rand = "GLMM Random"))) +
+               onestep_bi_rand = "One-Step GLMM",
+               twostep_binorm = "Two-Step Binomial Normal"))) +
   
   geom_errorbar(data =influence_df, 
                 aes(x = trial,
@@ -185,27 +207,30 @@ plt <- ggplot(influence_df,aes(x = trial , y = estimate) ) +
                     ymax=ci.ub)) +
   
   geom_hline(data = og_models, 
-             aes(yintercept = estimate),
+             aes(yintercept = estimate, color = model),
              linetype="dashed", 
-             color = "springgreen4",
+             
              size=0.75) +
   
   geom_rect(data = og_models, 
             inherit.aes = FALSE,
             aes(ymin=estimate.lb, ymax=estimate.ub, 
-                xmin = -Inf, xmax = Inf),
-            fill = "#3CBB7577",
+                xmin = -Inf, xmax = Inf,fill = model),
             alpha = 0.1) +
+  
+  scale_fill_npg()+
+  scale_color_npg()+
   
   theme(axis.line.y =element_blank(),
     axis.title.y =element_blank(),
     axis.ticks.y=element_blank(),
     strip.text.x = element_text(face = "bold" , colour = 'black' , size = 10.5),
     panel.spacing.x = unit(0.6 , 'cm'),
-    strip.background = element_rect(fill = NA, colour= NA)
+    strip.background = element_rect(fill = NA, colour= NA),
+    legend.position = 'none'
   )
 
-jpeg(filename = 'influenceplot.jpeg', res = 350, width=2750, height=4000 , units = 'px', pointsize = 10)
+jpeg(filename = './results/pooling_sa1.jpeg', res = 350, width=2750, height=4000 , units = 'px', pointsize = 10)
 
 plt
 
