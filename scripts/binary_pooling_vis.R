058bca16ce482df7748907e65f7e90572ed1b623
J-Baxter <james.baxter@ed.ac.uk>
2021-12-07 02:19:57 +0000

    updates to figure S5

--- scripts/binary_pooling_vis.R
+++ scripts/binary_pooling_vis.R
@@ -7,7 +7,9 @@
 # Dependencies
 source('./scripts/load_packages.R')
 source('./scripts/generalpurpose_funcs.R')
-
+require(ggplot2)
+require(ggsci)
+require(grDevices)
 
 # Plot for pseudo-bootstrap replicates of participant selection
 PltBoot <- function(data, intercept, ci.lb, ci.ub){
@@ -52,7 +54,7 @@ PltBoot <- function(data, intercept, ci.lb, ci.ub){
 # Import data
 influence_df <- read.csv("./results/pooling_sa1.csv") %>% arrange(., model)
 
-pooled_models <-  read.csv('./results/pooling_estsa2sa3sa4sa7.csv')
+pooled_models <-  read.csv('./results/pooling_estsa2sa3sa4sa6sa7.csv')
 
 resampled_models <- read.csv('./results/pooling_boot.csv')
 
@@ -66,8 +68,11 @@ og_models <- cbind("model" = pooled_models[1:2,1], pooled_models[1:2,3:8] %>% ro
 ###################################################################################################
 # Panel: Sensitivity analyses (exclusion criteria and resampling)
 
-# Exclusion critera dot and whisker plot
-figureS5_a <- ggplot(pooled_models,
+# S5A
+# Studies with (n<10) omitted, Studies with (p=0) omitted", Full analysis",'Only SGA sequences', 
+# 'Gold Standard Only'
+
+figureS5_a <- ggplot(pooled_models[1:8,],
                     aes(x= forcats::fct_rev(model), y = estimate, color = analysis)) +
   
   geom_point( shape = 4, 
@@ -96,8 +101,7 @@ figureS5_a <- ggplot(pooled_models,
     no_small = "Studies with (n<10) omitted",
     no_zeros = "Studies with (p=0) omitted",
     original = "Full analysis",
-    sga_only = 'Only SGA sequences',
-    no_extreme = 'Restricted to 11-37 genomes/patient')) + 
+    sga_only = 'Only SGA sequences')) + 
   
   theme(legend.position = c(0.8,0.86),
         axis.text = element_text(size = 9.5),
@@ -107,6 +111,8 @@ figureS5_a <- ggplot(pooled_models,
         #plot.margin = unit(c(2,4,2,1), "lines")
   )
 
+# S5B
+# Resampled Models
 figureS5_b  <- ggplot(resampled_models) + 
   geom_histogram(aes(x = estimate,
                  color=analysis, 
@@ -148,14 +154,60 @@ figureS5_b  <- ggplot(resampled_models) +
         legend.position = c(0.82,0.9),
         legend.background = element_blank())
 
+# S5C
+# Effect of number of genomes: Dot and whisker
+# All, no extreme, no small, no high
+figureS5_c <- ggplot(pooled_models[9:16,],
+                     aes(x= forcats::fct_rev(model), y = estimate, color = analysis)) +
+  
+  geom_point( shape = 4, 
+              size = 4,
+              position = position_dodge(0.5)) + 
+  
+  scale_y_continuous(name = "Probability of Multiple Founders",
+                     #limits=c(0,.5),
+                     expand = c(0.01, 0.01)) +
+  coord_cartesian(ylim = c(0,.5))+
+  
+  scale_x_discrete(name = "Model", 
+                   labels = c(
+                     onestep_bi_rand = "GLMM",
+                     twostep_binorm = "B-N"
+                   )) +
+  theme_bw() + 
+  
+  coord_flip() +
+  
+  geom_linerange(aes(ymin=estimate.lb, 
+                     ymax= estimate.ub, 
+                     color = analysis), 
+                 position = position_dodge(0.5)) +
+  
+  scale_color_npg(name = 'Analysis', labels = c(
+    original = "Full analysis",
+    gold_standard = "Restricted to 'gold-standard' Methodology",
+    no_extreme = 'Restricted to 11-37 genomes/patient',
+    no_smallgenomes = "Restriced to <11 genomes/patient",
+    no_largegenomes = "Restricted to >37 genomes/patient")) + 
+  
+  theme(legend.position = c(0.8,0.86),
+        axis.text = element_text(size = 9.5),
+        legend.text = element_text(size = 9.5),
+        axis.title = element_text(size = 11),
+        legend.background = element_blank()#,
+        #plot.margin = unit(c(2,4,2,1), "lines")
+  )
+
 
 figureS5 <- cowplot::plot_grid(figureS5_a, 
-                               figureS5_b , 
-                               ncol = 2,  rel_widths  = c(1,1) ,labels = "AUTO", align = 'h', axis = 'b', greedy = F)
+                               figureS5_b, 
+                               figureS5_c,
+                               ncol = 3,  rel_widths  = c(1,1,1) ,labels = "AUTO", align = 'h', axis = 'b', greedy = F)
 
 
-# Save to file (ggsave rather than setEPS() to presever transparencies)
-ggsave("./results/figureS5.eps", device=cairo_ps,width = 16, height = 10, units= 'in')
+
+# Save to file (ggsave rather than setEPS() to preseve transparencies)
+ggsave("./results/figureS5.eps", device=cairo_ps, width = 16, height = 10, units= 'in')
 figureS5
 dev.off()
 
