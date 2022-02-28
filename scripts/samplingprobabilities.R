e24e37914a32b7f528db6e0c0ffc57684fa52124
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2022-02-28 11:54:02 +0000

    init

---
+++ scripts/samplingprobabilities.R
@@ -0,0 +1,85 @@
+# Simulating the effect of sampling density on the observed number of founder lineages
+
+# Dependencies
+library(actuar)
+library(ggplot2)
+library(cowplot)
+
+
+# Dual founder infection with variable ratio between major/minor variant
+Binom <-function(sample, prop_major){
+  prop_minor <- 100 - prop_major 
+  P_same <- (choose(prop_major,sample)+choose(prop_minor,sample))/choose(100,sample)
+  P <- 1-P_same
+  return(P)
+}
+
+
+# Zero-truncated binomial with varying probability (p) of selecting any given variant
+ZT_Binom <- function(sample, p = 0.05){
+  require(actuar)
+  
+  prob_1 <- dztbinom(1, sample, prob = p, log = FALSE)
+  out <- 1-  prob_1
+  return(out)
+}
+
+
+set.seed(4472)
+sample_size <- 1:30
+
+
+# Binomial 
+maj_props <-  seq(5,50, by = 5)
+binom_probs <- sapply(sample_size, function(x) sapply(maj_props, Binom, sample = x)) %>% 
+  as.data.frame() %>% 
+  gather(key = 'sample_size', value = 'prob_both') %>% 
+  cbind.data.frame(maj_proportion = seq(5,50, by = 5)) %>% 
+  mutate(sample_size = replace(sample_size, values = rep(1:30, each = 10)))
+
+binom_probs$sample_size <- as.numeric(binom_probs$sample_size)
+
+plt_a <- ggplot(data = binom_probs,
+                aes(x = sample_size, y =  prob_both, group= maj_proportion , color = maj_proportion)) + 
+  geom_line() +
+  scale_x_continuous(name = 'Sample Size', expand = c(0,0))+
+  scale_y_continuous(name = 'P[Both Lineages Sampled]', expand = c(0,0)) +
+  scale_color_viridis_c(name = 'Proportion of Major Variant (%)') +
+  theme_classic() +
+  theme(legend.position = 'none')
+
+
+# Zero-truncated Binomial
+p <- seq(0.05,0.8,by = 0.05)
+
+zt_probs <- sapply(sample_size, function(x) sapply(p, ZT_Binom, sample = x)) %>% 
+  as.data.frame() %>% 
+  gather(key = 'sample_size', value = 'prob_more') %>% 
+  cbind.data.frame(p = seq(0.05,0.8,by = 0.05)) %>% 
+  mutate(sample_size = replace(sample_size, values = rep(1:30, each = 16)))
+
+zt_probs$sample_size <- as.numeric(zt_probs$sample_size)
+
+plt_b <- ggplot(data =zt_probs,
+                aes(x = sample_size, y =  prob_more, group= p , color = p)) + 
+  geom_line() +
+  scale_x_continuous(name = 'Sample Size', expand = c(0,0))+
+  scale_y_continuous(name = 'P[Finding >1 Lineages]', expand = c(0,0)) +
+  scale_color_viridis_c(name = 'P[X=x]') +
+  theme_classic()+
+  theme(legend.position = 'none')
+
+
+# Format output
+fig <- plot_grid(plt_a + theme(legend.position = c(0.8, 0.25)), 
+                 plt_b + theme(legend.position = c(0.8, 0.25)), 
+                 align = 'hv', labels = 'AUTO')
+
+
+# Save to file
+setEPS()
+postscript("./results/samplingprobs.eps", width = 16, height = 10)
+fig
+dev.off()
+
+## END ##
